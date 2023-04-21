a9975ec33ffd5d8f4b5add176ed98f27f3a13fd9
Natalia Garcia Sanchez <natalia.garcia.sanchez@alumnos.upm.es>
2023-04-21 14:06:29 +0200

    updated first NGS scripts for HPC functionality with Slurm

--- Code Project Repository (CPR)/RNAseq_pipeline_first_steps/download_map_assemble.sh
+++ Code Project Repository (CPR)/RNAseq_pipeline_first_steps/download_map_assemble.sh
@@ -3,65 +3,100 @@
 # Download ensembl assembly into directory of choice and create hisat2 mapping
 # Author : Natalia García Sánchez
 # Date : 24/02/2023
-# --- 
+# ---
 
+
+# Bash output color parameter for user interaction
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "\n${GREEN} ~~~~~~~~~~~~ download_map_assemble.sh ~~~~~~~~~~~~\n${NC}"
+echo -e "A script for HISAT2 read alignment to Brassica napus reference genome and assembly"
+echo -e "\n\n ${GREEN} USAGE : ${NC} bash download_map_assemble.sh ${GREEN} <trimmed_fq_read_directory_path>* <numsamples_per_condition> ${NC} \n\n\t (*) : OPTIONAL arg - IF NOT PROVIDED IN COMMAND LINE, ASKED IN USER INPUT"
+
+
+# Loading necessary modules
+echo -e "${GREEN}\n------------------------------------\nLoading Python, HISAT2, SAMTOOLS, AGAT, PERL, GCC, StringTie dependencies\n------------------------------------${NC}"
+module load Python/3.8.6-GCCcore-10.2.0
 module load GCCcore/10.2.0
 module load Perl/5.32.0-GCCcore-10.2.0-minimal
 module load gzip/1.10-GCCcore-8.2.0
-HTSeq/0.11.1-foss-2019a
+module load HTSeq/0.11.1-foss-2019a
 module load HISAT2/2.1.0-foss-2019a
-
+module load SAMtools/1.9-GCC-8.2.0-2.31.1
+module load StringTie/2.0.5-GCCcore-8.2.0
+module load AGAT/0.9.2-GCC-11.2.0
 
 # Download reference genome ensembl assembly in fasta format
 
-# Uncomment if you'd rather use the latest ncbi b napus accession assembly
-#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/020/379/485/GCF_020379485.1_Da-Ae/GCF_020379485.1_Da-Ae_genomic.fna.gz
-#gzip -d GCF_020379485.1_Da-Ae_genomic.fna.gz
-#mv GCF_020379485.1_Da-Ae_genomic.fna genome.fa
-
-echo "1) Downloading B napus ensembl assembly"
+echo -e "${GREEN}\n------------------------------------\n1) Downloading B napus ensembl assembly genome\n------------------------------------${NC}"
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/751/015/GCA_000751015.1_AST_PRJEB5043_v1/GCA_000751015.1_AST_PRJEB5043_v1_genomic.fna.gz
 gzip -d GCA_000751015.1_AST_PRJEB5043_v1_genomic.fna.gz
-mv GCA_000751015.1_AST_PRJEB5043_v1_genomic.fna.gz genome.fa
+mv GCA_000751015.1_AST_PRJEB5043_v1_genomic.fna genome.fa
 
 # Download ensembl annotation in GFF format and convert 
-echo "Ensembl annotation GTF file will be downloaded"
-echo "Downloading B napus ensembl plants annnotation GFF3 file"
+
+echo -e "${GREEN}\n------------------------------------\n2)Downloading Brassica napus Ensembl Plants GFF3 file annotation and converting into GTF file \n------------------------------------${NC}"
 wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-56/gff3/brassica_napus/Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz
 gzip -d Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz
 echo "Converting GFF3 file to GTF3"
-agat_convert_sp_gff2gtf.pl --gff Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz -o genome.gtf
+agat_convert_sp_gff2gtf.pl --gff Brassica_napus.AST_PRJEB5043_v1.56.gff3 -o genome.gtf 2>tmp.conversion.log
+
 
 # Extract genomic sequences corresponding to exon/ss sequences
-echo "2) Extracting splice sites and exon sequences"
-echo "write directory path to hisat2 scripts hisat2_extract_splice_sites and hisat2_extract_exons"
-path_to_hisat=read()
-"$path_to_hisat"/hisat2_extract_splice_sites.py genome.gtf > genome.ss
-"$path_to_hisat"/hisat2_extract_exons.py genome.gtf > genome.exon
 
+echo -e "${GREEN}3) Extracting splice sites and exon sequences${NC}"
+path_to_hisat="$(whereis hisat2 | grep -oP "/.+/bin")/hisat2"
+
+echo -e "${GREEN}\n------------------------------------\nDownloading and using python helper scripts to extract splice sites and exons in reference genome fasta file\n------------------------------------${NC}"
+
+wget https://raw.githubusercontent.com/DaehwanKimLab/hisat2/master/hisat2_extract_splice_sites.py
+chmod 755 hisat2_extract_splice_sites.py
+python3 hisat2_extract_splice_sites.py genome.gtf > genome.ss
+
+wget https://raw.githubusercontent.com/DaehwanKimLab/hisat2/master/hisat2_extract_exons.py
+chmod 755 hisat2_extract_exons.py
+python3 hisat2_extract_exons.py genome.gtf > genome.exon
+
+echo -e "${GREEN}\n------------------------------------\n4)Align to reference genome with splice sensitive HISAT2 alignment\n------------------------------------${NC}"
 
 # Checking read data 
-echo "> Enter clean read data directory path : \n>"
-path_to_clean_reads=read()
 
-# Checking read data 
-echo "> Enter data directory path where you want to store your indexing and bam files: \n>"
-path_to_bam=read()
+if [ $# -eq 0 ]; then  
+	
+	echo -e "\n------------------------------------\n No command line arguments found, asking for user input... \n------------------------------------\n"
+	echo -e "\n${GREEN}> Enter clean read data directory path : \n>${NC}"
+	read path_to_clean_reads
 
-# Checking read data 
-echo "> Enter data directory path where you want to store bam files flagstat_reports: \n>"
-path_to_flagstat=read()
+	echo -e "\n${GREEN}> Enter number of samples : \n >${NC}"
+	read fnumbers
+fi
+
+# Making output directories
+
+echo -e "${GREEN}\n------------------------------------\nMaking bam_output assembly_output and flagstat_output directories${NC}\n------------------------------------" 
+if ! [ -d "bam_output" ] || ! [ -d "flagstat_output" ] || ! [ -d "assembly_output" ];then
+mkdir bam_output
+mkdir assembly_output
+mkdir flagstat_output
+fi
+
+path_to_bam="bam_output"
+path_to_flagstat="flagstat_output"
+path_to_assembly="assembly_output"
+
 
 if [ -d "$path_to_clean_reads" ]
 then
 
-		if [ $(ls *.fq | wc -l) -gt 0 ]; 
+	if [ $(ls "$path_to_clean_reads"/*.fq | wc -l) -gt 0 ]; 
 		then 
 			echo "Clean read files found in directory. Proceeding to mapping...";
 		
 		else
 			echo "No clean read files found. \nExiting..."
 			exit 1
+		fi
 	else
 		echo "Clean reads directory is empty. Try again with a proper dir"
 	exit 1
@@ -73,30 +108,50 @@ fi
 
 # -- Creating genome index wit BW comp algorithm: This will create a series of indexing files needed
 
-hisat2-build -p 16 --exon genome.exon --ss genome.ss genome.fa "$path_to_bam"/genome_tran.idx
+echo -e "${GREEN}\n------------------------------------\n 5) Creating index files accounting for splicesites and exonic sequences in reference genome \n------------------------------------${NC}"
 
-# -- Mapping to index genome and converting formats
+srun -p medium -t 00:40:00 --mem 200GB hisat2-build -p 16 --exon genome.exon --ss genome.ss genome.fa "$path_to_bam"/genome_tran.idx
+
+# -- Mapping to index genome indexing and converting SAM into a sorted bam in a single nested for loop
+
+echo -e "${GREEN}\n------------------------------------\n 6) Read alignment to reference genome and conversion into sorted BAM output \n------------------------------------${NC}"
 
 array_condition=(C T)
 
 for condition in "${array_condition[@]}"
 do
-	for sample_num in {1..3}
+	for (( sample_num=1; sample_num<="$fnumbers"; sample_num++ ))
 	do
 		# Evaluate mappings to the indexed genome
 		# Convert SAM mappings into Binary BAM format and not creating SAM file by pipe streaming
 		# Evaluating mapping with 
 		# Sort BAM for further downstream analyses
 
-		hisat2 -x genome_tran.idx \
-		-1 "$path_to_clean_reads"/"$condition"rep_"$sample_num"_1.trimmed.fq \
-		-2 "$path_to_clean_reads"/"$condition"rep_"$sample_num"_2.trimmed.fq | \
-		  tee >(samtools flagstat - > "$path_to_flagstat"/"$condition"rep_"$sample_num".flagstat) | \
+		srun -p long -t 1:00:00 --mem 128GB hisat2 -x "$path_to_bam"/genome_tran.idx \
+		-1 "$path_to_clean_reads"/"$condition"rep"$sample_num"_1.trimmed.fq \
+		-2 "$path_to_clean_reads"/"$condition"rep"$sample_num"_2.trimmed.fq | \
 		  samtools sort -O BAM | \
-		  tee "$path_to_bam"/"$condition"rep_"$sample_num".bam | \
-		  samtools index - "$path_to_bam"/"$condition"rep_"$sample_num".bam.bai
+		  tee "$path_to_bam"/"$condition"rep"$sample_num".bam | \
+		  samtools index - "$path_to_bam"/"$condition"rep"$sample_num".bam.bai
 	done
 done
+
+# Make assessments of HISAT2 alignment in each biological replicate
+
+echo -e "${GREEN}\n------------------------------------\n 7) Make read alignment reports \n------------------------------------${NC}"
+
+for file in "$path_to_bam"/*.bam; 
+do 
+	filename=$(basename "$file")
+	samtools flagstat "$file" >"$path_to_flagstat"/${filename%.bam}.flagstat 2>"$path_to_flagstat"/${filename%.bam}.flagstat.log
+done
+
+
+# Assemble mapped reads into GTF format taking a reference
+
+echo -e "${GREEN}\n------------------------------------\n 8) Assemble and annotate mappings into GTF format using the Ensembl Plants GTF reference functional annotation file \n------------------------------------${NC}"
+
+stringtie -B -G genome.gtf -o "$path_to_assembly"/Bna_final_assemble_HDAC.gtf "$path_to_bam"
 
 
 
--- Code Project Repository (CPR)/RNAseq_pipeline_first_steps/read_count.sh
+++ Code Project Repository (CPR)/RNAseq_pipeline_first_steps/read_count.sh
@@ -5,114 +5,111 @@
 # Date : 24/02/2023
 # ---
 
+# Bash output color parameter for user interaction
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "\n${GREEN} ~~~~~~~~~~~~ read_count.sh ~~~~~~~~~~~~\n${NC}"
+echo -e "A script for read count using HISAT read alignment output BAM files"
+echo -e "\n\n ${GREEN} USAGE : ${NC} bash download_map_assemble.sh ${GREEN} \n\tpath_to_bam and genome files are provided in user input.${NC}"
+
+help(){
+echo -e echo -e "\n${GREEN} ~~~~~~~~~~~~ read_count.sh ~~~~~~~~~~~~\n${NC} USAGE : path_to_bam and genome files are provided in user input\n bam directory required can optionally contain custom indexes."
+exit 1
+}
+
+# Loading necessary modules
+echo -e "${GREEN}\n------------------------------------\nLoading SAMTOOLS, AGAT, PERL, GCC, HTSeq dependencies\n------------------------------------${NC}"
+module load Python/3.8.6-GCCcore-10.2.0
+module load GCCcore/10.2.0
+module load Perl/5.32.0-GCCcore-10.2.0-minimal
+module load gzip/1.10-GCCcore-8.2.0
+module load HTSeq/0.11.1-foss-2019a
+module load SAMtools/1.9-GCC-8.2.0-2.31.1
+module load AGAT/0.9.2-GCC-11.2.0
+
 # RETRIEVING AND CHECKING INPUT DATA
 
 # Asking for dir path with input bam files and checking if dir is empty
 
-echo "Enter directory path to bam files: \n>"
-path_to_bam_files=read()
+echo -e "${GREEN}\n------------------------------------\n1) Checking bam and genome files provided in user input \n-------------------${NC}"
+echo -e "${GREEN}Enter directory path to bam and bam.bai (index) files: \n>${NC}"
+read path_to_bam_files
 
 if [ -d "$path_to_bam_files" ]
 then
 	echo "Bam file directory exists"
 	
-	if [ $(ls *.bam | wc -l) -gt 0 ]; 
+	if [[ $(ls "$path_to_bam_files"/*.bam | wc -l) -gt 0 ]] 
 	then 
-		echo "Bam files found in directory. Proceeding to read count";
-	
+		echo "Bam files found in directory. Proceeding to checking index files."
+		
+		if [[ $(ls "$path_to_bam_files"/*.bam.bai | wc -l) -gt 0 ]]
+		then
+			echo -e "\n-Found index files in directory. Proceeding to read count..."
+		else 
+			echo -e "\n-No index files were found. Proceeding to indexing files and read count"
+			
+			for file in "$path_to_bam_files"/*.bam
+			do
+				ifilename=$(basename "$file")
+				echo "dudududu $ifilename"	 
+				#samtools index "$file" >"$ifilename".bai
+			done
+		fi
+
 	else
-		echo "No bam files found. \nExiting..."
-		exit 1
+		echo -e "No bam files found. \nExiting..."
+		help
 
 	fi
 
 else
-	echo "Bam file directory is empty. Try again with a proper dir. \nExiting..."
-	exit 1
+	echo -e "Bam file directory is empty. Try again with a proper dir. \nExiting..."
+	help
 
 fi
 
 
 # Ask if GTF annotation file is present. Otherwise Download latest annotation in GTF format
 
-echo "Do you have an annotation file available (y/n)"
+echo -e "${GREEN}Do you have an annotation file available (y/n)${NC}"
 read response
-case $response in
+case "$response" in
 	Y|y|[Yy]es)
-		echo "Provide GTF filepath"
+		echo -e "${GREEN}Provide GTF filepath\n>${NC}"
 		read path_to_genome
 	;;
 	N|n|[Nn]o)
-	    echo "Ensembl annotation GTF file will be downloaded"
-	  	echo "Downloading B napus ensembl plants annnotation GFF file"
-	  	wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-56/gff3/brassica_napus/Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz
-	  	gzip -d Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz
-	  	echo "Converting GFF3 file to GTF3"
-	  	agat_convert_sp_gff2gtf.pl --gff Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz -o genome.gtf
+	    	echo -e "${GREEN}\n------------------------------------\nDownloading Brassica napus Ensembl Plants GFF3 file annotation and converting into GTF file \n--------------------------------${NC}"
+                wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-56/gff3/brassica_napus/Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz
+		gzip -d Brassica_napus.AST_PRJEB5043_v1.56.gff3.gz
+		echo "Converting GFF3 file to GTF3"
+		agat_convert_sp_gff2gtf.pl --gff Brassica_napus.AST_PRJEB5043_v1.56.gff3 -o genome.gtf 2>tmp.conversion.log
 
-		#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/3708/102/GCF_020379485.1_Da-Ae/GCF_020379485.1_Da-Ae_genomic.gtf.gz
-		#gzip -d GCF_020379485.1_Da-Ae_genomic.gtf.gz
-		#mv GCF_020379485.1_Da-Ae_genomic.gtf.gz genome.gtf
-		path_to_genome=$(pwd)"/genome.gtf"
 	;;
 	  *)
 	    echo "Irrelevant response. Exiting"
 	    exit 0
 	;;
-	esac
+esac
 
-
-# SORTING AND MAKING REPORTS OF BAM FILES
-
-# Reading bam file with samtools
-
-conda source activate
-conda activate TFM
-mkdir sorted_bam
-mkdir flagstat_reports
-
-for file in $path_to_bam_files/*.bam; do 
-	# Sorting by position
-	samtools sort $file >sorted_bam/$file
-done
-
-cd sorted_bam
-
-#Performing flagstat 
-for file in *.bam; do samtools flagstat $file >../flagstat_reports/${file%.bam}.flagstat_report.txt; done
-
-cd ..
-
-# take first column from flagstat                                               
-cat $(ls flagstat_reports/*flagstat_report*.txt | head -n1) |  sed 's/\s0\s/\s0\t/g' | cut -d"$(echo -e "\t")" -f2 | sed 's/(.*)//g' >flagstat_reports/flagstat_report
-
-# Join all flagstat reports into a single document and remove the single files  
-
-for file in flagstat_reports/*.flagstat_report.txt; do 
-
-	# Get only information about reads into tmp_file
-	cat $file |  sed 's/\s0\s/\s0\t/g' | cut -d"$(echo -e "\t")" -f1 >flagstat_reports/tmp_file
-
-	join flagstat_reports/flagstat_report flagstat_reports/tmp_file; 
-
-	done
-
-rm flagstat_reports/*.flagstat_report.txt
-
-
-# Adding header
-printf "MAPPING\t" >flagstat_reports/flagstat_report
-echo "$(ls flagstat_reports/*flagstat_report*.txt)" | tr "\n" "\t" >>flagstat_reports/flagstat_report
-
-
-# INDEXING FILES PRE-READ COUNT
-
-for file in $path_to_bam_files/*.bam; do samtools index $file; done
+if [[ ! -d read_count_output ]]
+then
+mkdir read_count_output
+fi
 
 # --- Performing htseq count for genes
-
-for file in $path_to_bam_files/*.bam; 
-do 
-	htseq-count -i gene_id -s no -r pos -f bam $file $path_to_genome -m union -t exon >${file%.bam}".counts"; 
+echo -e "\n${GREEN}\n------------------------------------\n2) Performing read count\n------------------------------------\n${NC}"
+for file in "$path_to_bam_files"/*.bam 
+do
+    cfilename=$(basename "$file")
+    srun -p long -t 1:00:00 --mem 128GB samtools view -h "$file" | htseq-count -i gene_id -s no -r pos - "$path_to_genome" -m union -t exon >read_count_output/"${cfilename%.bam}".counts 
 
 done
+
+
+
+
+
+
